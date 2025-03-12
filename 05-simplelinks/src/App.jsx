4c35b07f3bc98087f9ebcf504c550ee38085d029
Luiz G. <luiz.mwlo76@gmail.com>
2025-03-12 16:21:57 -0300

    adição da função de pausar/retormar a música da landing page.

--- 05-simplelinks/src/App.jsx
+++ 05-simplelinks/src/App.jsx
@@ -1,46 +1,62 @@
-import { useRef, useState } from 'react'
-import music from './assets/music/intro_audio.mp3'
-import './App.css'
-
+import { useRef, useState } from "react";
+import music from "./assets/music/intro_audio.mp3";
+import "./App.css";
 
 function App() {
   const audioRef = useRef(null);
+  const [isPlaying, setIsPlaying] = useState(true);
 
-  const playMusic = () =>{
+  const playMusic = () => {
     if (audioRef.current) {
       audioRef.current.play();
+      setIsPlaying();
     }
-  }
+  };
+  const pauseMusic = () => {
+    if (audioRef.current) {
+      audioRef.current.pause();
+      !setIsPlaying()
+    }
+  };
+
+  const trocaPlayPause = () => {
+    isPlaying ? pauseMusic() : playMusic();
+    const btn = document.getElementById("btn-music");
+    if (btn) {
+      btn.classList.toggle("playing", !isPlaying);
+    }
+  };
 
   return (
     <div id="App">
-    
+      <div className="stars"></div>
+      <div className="Profile">
+        <img src="https://placehold.co/200x200"></img>
+        <p>@LLuizXL</p>
+        <audio ref={audioRef} loop>
+          <source src={music} type="audio/mp3" />
+        </audio>
 
-       
-      <div className='stars'></div>
-         <div className='Profile'>
-          <img src='https://placehold.co/200x200'></img>
-          <p>@LLuizXL</p>
-          <audio ref={audioRef} loop>
-          <source src={music} type='audio/mp3' />
-          </audio>
+        <button id="btn-music" onClick={trocaPlayPause}></button>
+      </div>
 
-          
-          <button onClick={playMusic}></button>
-             
-         </div>
+      <div className="Menu">
+        <ul>
+          <li>
+            <a href="">Regras</a>
+          </li>
+          <li>
+            <a href="">Projetos</a>
+          </li>
+          <li>
+            <a href="">Sobre mim</a>
+          </li>
+        </ul>
+      </div>
 
-         <div className='Menu'>
-          <ul>
-            <li><a href="">Regras</a></li>
-            <li><a href="">Projetos</a></li>
-            <li><a href="">Sobre mim</a></li>
-          </ul>
-         </div>
-     
-
+      <footer>footer</footer>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
